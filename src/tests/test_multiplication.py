32a8d0b6187066c6bc5f89c271ecd57252c9a9d4
Bakary Seydou Diaby <bakaryseydou@gmail.com>
2021-08-08 19:20:10 +0200

    modification tests multi et soustraction avec insertion volontaire test faux

--- src/tests/test_multiplication.py
+++ src/tests/test_multiplication.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # test_addition.py
 
-# importation du module add du package src/calculatrice.py
+# importation du module multiply du package src/calculatrice.py
 import sys
 
 sys.path.append("/mnt/c/Users/Utilisateur/Desktop/bak_projet/2_docker_python_jenkins/python_docker_jenkins_git/src")
@@ -12,7 +12,7 @@ import pytest
 
 def test_multiply():
     result = multiply(3, 4)
-    assert result == 12
+    assert result == 10
 
 
 def test_multiply_string():
--- src/tests/test_soustraction.py
+++ src/tests/test_soustraction.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # test_addition.py
 
-# importation du module add du package src/calculatrice.py
+# importation du module substract du package src/calculatrice.py
 import sys
 
 sys.path.append("/mnt/c/Users/Utilisateur/Desktop/bak_projet/2_docker_python_jenkins/python_docker_jenkins_git/src")
@@ -12,7 +12,7 @@ import pytest
 
 def test_subtract_positive():
     result = subtract(4, 3)
-    assert result == 1
+    assert result == 10
 
 
 def test_subtract_negative():
